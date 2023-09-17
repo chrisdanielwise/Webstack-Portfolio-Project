5a8ef2da09362ee1c642539baee4d12655951e72
chrisdanielwise <chrisdanielwise@gmail.com>
2023-09-17 04:59:59 +0100

    made some correction to th e backend

--- backend/api/routes/agents.js
+++ backend/api/routes/agents.js
@@ -21,7 +21,7 @@ router.put("/:id", verify, async (req, res) => {
         },
         { new: true }
       );
-      res.redirect("/frontend/housify/src/components/templates/LoginForm.js");
+      res.redirect("/frontend/homehub/src/components/templates/LoginForm.js");
     } catch (err) {
       res.status(500).json(err);
     }
